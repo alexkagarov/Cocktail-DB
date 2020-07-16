2b5e5d778626c500801851662fd933340950154b
Alex Kagarov <alexkagarov@gmail.com>
2020-07-16 16:59:12 +0300

    Initial Commit

---
+++ Cocktail DB/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Cocktail DB
+//
+//  Created by Mac on 16.07.2020.
+//  Copyright © 2020 hialex. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
